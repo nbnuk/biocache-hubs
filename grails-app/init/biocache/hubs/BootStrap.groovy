2c6da2f5e5f3f8572cd75490f067a0336a3fbcf7
Helen Manders-Jones <realhelenmjones@gmail.com>
2022-06-24 08:26:23 +0100

    extract OccurrenceController.groovy

--- grails-app/init/biocache/hubs/BootStrap.groovy
+++ grails-app/init/biocache/hubs/BootStrap.groovy
@@ -1,9 +1,16 @@
 package biocache.hubs
 
+import au.org.ala.biocache.hubs.SearchRequestParams
+
 class BootStrap {
 
     def init = { servletContext ->
+        initSearchRequestParams();
     }
     def destroy = {
+    }
+
+    static def initSearchRequestParams = {
+        SearchRequestParams.metaClass.nbnRequiredFacets = [] as String[]
     }
 }
