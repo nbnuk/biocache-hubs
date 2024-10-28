e011c85b2564376566eeef8877c3823309e56033
Anthony Ross <tony.ross@mac.com>
2024-10-28 20:16:54 +0100

    added method

--- grails-app/services/au/org/ala/biocache/hubs/WebServicesService.groovy
+++ grails-app/services/au/org/ala/biocache/hubs/WebServicesService.groovy
@@ -119,11 +119,6 @@ class WebServicesService {
         return getJsonElements(url, "${grailsApplication.config.alerts.apiKey}")
     }
 
-    def getSaveSearches(String userId) {
-        def url = "${grailsApplication.config.alerts.baseUrl}" + "/api/savedSearch/list/" + userId
-        return getJsonElements(url, "${grailsApplication.config.alerts.apiKey}")
-    }
-
     def subscribeMyAnnotation(String userId) {
         String url = "${grailsApplication.config.alerts.baseUrl}" + "/api/alerts/user/" + userId + "/subscribeMyAnnotation"
         postFormData(url, [:], grailsApplication.config.alerts.apiKey as String)
